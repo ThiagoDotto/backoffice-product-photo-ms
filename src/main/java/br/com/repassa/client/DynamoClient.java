56dbf77efda81439d6a3d05d7b44f98fa24a3761
cmedger <carlos.medger@gmail.com>
2023-10-11 12:06:37 -0300

    melhoria conexao dynamo

--- src/main/java/br/com/repassa/client/DynamoClient.java
+++ src/main/java/br/com/repassa/client/DynamoClient.java
@@ -7,22 +7,30 @@ import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 public class DynamoClient {
 
-	public DynamoDbClient openDynamoDBConnection() {
-        try {
-            var accessKey = "AKIA3GWR6GFBSXT2QB52";
-            var secretKey = "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";
-            AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKey, secretKey);
-
-            DynamoDbClient client = DynamoDbClient.builder()
-                    .region(Region.US_EAST_1)
-                    .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
-                    .build();
-
-            return client;
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return null;
-        }
+	private static DynamoDbClient clientInstance;
+	
+	public static synchronized DynamoDbClient openDynamoDBConnection() {
+		 if (clientInstance == null) {
+			 System.out.println("CRIOU CONEXAOO >>>>" );
+	        try {
+	            var accessKey = "AKIA3GWR6GFBSXT2QB52";
+	            var secretKey = "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";
+	            AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKey, secretKey);
+	
+	            clientInstance = DynamoDbClient.builder()
+	                    .region(Region.US_EAST_1)
+	                    .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
+	                    .build();
+	
+	        } catch (Exception e) {
+	            System.out.println(e.getMessage());
+	            return null;
+	        }
+		 } else {
+			 System.out.println("NAO CRIOU CONEXAOO >>>>" );
+		 }
+		 
+		return clientInstance;
     }
 
     public void closeDynamoDbConnection(DynamoDbClient dynamoDB) {

f36451660bec12f98b7773d7173d96f0dd92b67c
Julio Cézar <julio.ciec@gmail.com>
2023-11-10 17:20:57 -0400

    Ajustando o caminho das novas fotos

--- src/main/java/br/com/repassa/service/PhotosService.java
+++ src/main/java/br/com/repassa/service/PhotosService.java
@@ -263,7 +263,7 @@ public class PhotosService {
         var photosValidate = new PhotosValidate();
         AtomicReference<String> urlImage = new AtomicReference<>(new String());
         String username = StringUtils.replaceCaracterSpecial(StringUtils.normalizerNFD(name));
-        var objectKey = photosValidate.validatePathBucket(username, imageDTO.getDate());
+        var objectKey = photosValidate.validatePathBucket(name, imageDTO.getDate());
         AtomicReference<PhotosManager> photosManager = new AtomicReference<>(new PhotosManager());
 
         for (var i = 0; i < imageDTO.getPhotoBase64().size(); i++) {

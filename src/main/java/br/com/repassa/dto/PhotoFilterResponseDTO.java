005efe9853323a2dc64932bcb8e538a78d1a97da
thiago <tdotto@gmail.com>
2023-12-04 10:49:39 -0300

    #24167 url_thumbnail

--- src/main/java/br/com/repassa/dto/PhotoFilterResponseDTO.java
+++ src/main/java/br/com/repassa/dto/PhotoFilterResponseDTO.java
@@ -44,8 +44,7 @@ public class PhotoFilterResponseDTO implements Serializable {
             photoFilterResponseDTO.setIsValid(item.get("is_valid").s());
             photoFilterResponseDTO.setOriginalImageUrl(item.get("original_image_url").s());
             photoFilterResponseDTO.setSizePhoto(item.get("size_photo").s());
-            // TODO: ISSO tem que ser alterado com a modificação do PBI-24176
-            photoFilterResponseDTO.setUrlThumbnail(item.get("thumbnail_base64").s());
+            photoFilterResponseDTO.setUrlThumbnail(item.get("url_thumbnail").s());
             photoFilterResponseDTO.setUploadDate(item.get("upload_date").s());
             // photoFilterResponseDTO.setId((item.get(PhotoAbstractService.FRUIT_NAME_COL).s());
             // photosManager.setDescription(item.get(AbstractService.FRUIT_DESC_COL).s());
